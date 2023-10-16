e7553eb450a2ba3224f50a9693c3fcd6f0017779
Daniel Brunner <info@daniel-brunner.at>
2023-10-16 19:19:25 +0200

    More basics

--- 01-intro/index.php
+++ 01-intro/index.php
@@ -26,6 +26,50 @@
         } else {
             echo 'Kleiner!';
         }
+
+        echo '<br>';
+
+        // Variablen
+        $mySexyVar = 'Daniel'; // Variablen beginnen mit $
+        $MY_CONSTANT = 14.23; // Schema für Konstanten - großschreiben
+
+        // Schleifen in PHP
+        for ($i = 0; $i < 10; $i++) {
+            echo $i . '<br>';
+        }
+
+        echo '<br>';
+
+        // Arrays
+        $myArray = [12, 0, 'lol', '', true, false, null];
+
+        // Alle Elemente im Array durchloopen
+        // count() gibt uns die Anzahl an Elementen in meinem Array. Wie .length in JS
+        for ($i = 0; $i < count($myArray); $i++) {
+            echo $myArray[$i] . '<br>'; // Vorsicht bei der Ausgabe von falsy Werten, werden unterdrückt :(
+            // var_dump($myArray[$i]); // Mit var_dump detailliertere Ausgabe
+        }
+
+        // Objekte in PHP gibt es als assoziative Array's :)
+        $student = [
+            'name' => 'Daniel',
+            'isPaying' => true,
+            'grades' => [
+                'midterm' => 81,
+                'final' => 105,
+            ],
+        ];
+
+        // echo $student; // Array's kann ich so nicht ausgeben
+        // print_r($student); // Geht, aber nicht so gut
+        // var_dump($student); // var_dump geht immer, aber Output etwas meh
+
+        // Kombi schon etwas besser
+        echo '<pre>';
+
+        // var_dump($student);
+        print_r($student);
+        echo '</pre>';
     ?>
 
     <!-- Nach dem schließenden PHP-Tag kann ich hier wieder HTML schreiben -->
